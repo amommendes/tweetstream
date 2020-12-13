b8ec143c7579a379d4e7d6a35657d2be68264d71
amommendes <amommendes@hotmail.com>
2020-12-13 15:30:38 -0300

    :sparkles: Finish spark streaming

--- run_streaming_job.py
+++ run_streaming_job.py
@@ -2,7 +2,14 @@ from tweetstream.consumers.twitter_streaming import TwitterStreamingConsumer
 from tweetstream.clients.spark import SparkClient
 
 def main():
-    spark_client = SparkClient()
+    spark_client = SparkClient(
+        session_params={
+            "spark.jars":
+                "/home/amomluiz/projects/tweetstream/libs/spark-sql-kafka-0-10_2.11-2.4.5.jar,"
+                "/home/amomluiz/projects/tweetstream/libs/kafka-clients-2.4.0.jar",
+            "failOnDataLoss": "false"
+        }
+    )
     spark = spark_client.get_session()
     consumer = TwitterStreamingConsumer(spark=spark)
     consumer.start()
--- tweetstream/consumers/twitter_streaming.py
+++ tweetstream/consumers/twitter_streaming.py
@@ -6,7 +6,7 @@ logger.basicConfig()
 
 class TwitterStreamingConsumer():
 
-    def __init__(self, spark, output_path="file:///tmp/consumer", format="parquet", host="localhost", port="3333"):
+    def __init__(self, spark, output_path="file:///tmp/consumer", format="parquet", host="localhost", port="9092"):
         self.spark = spark
         self.output_path = output_path
         self.format = format
@@ -17,16 +17,29 @@ class TwitterStreamingConsumer():
         logger.info("Creating read stream")
         tweets_df = self.spark \
             .readStream \
-            .format("socket") \
-            .option("host", self.host) \
-            .option("port", self.port) \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", ":".join([self.host, self.port])) \
+            .option("startingOffsets", "earliest") \
+            .option("subscribe", "twitter") \
             .load()
 
+        logger.info("Converting value")
+        tweets_df_cast = tweets_df.selectExpr(
+            "CAST(value AS STRING) as value",
+            " CAST(key AS STRING) as key",
+            "topic",
+            "`timestamp`"
+        )
         logger.info("Writing stream")
-        tweets_df.writeStream. \
+
+        writer = tweets_df_cast.\
+            writeStream. \
             format(self.format). \
             option("path", self.output_path). \
-            option("checkpointLocation", self.output_path+"/checkpoint"). \
-            trigger(processingTime='2 seconds'). \
+            option("checkpointLocation", "/tmp/checkpoint"). \
+            trigger(processingTime='15 seconds'). \
             start()
+
+        logger.info(f"Writing status {writer.status}")
+        writer.awaitTermination()
 
