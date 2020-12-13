ff4a9653557a4456573a99934d8dc0d7a83f2937
amommendes <amommendes@hotmail.com>
2020-12-13 02:40:33 -0300

    :art: Sending entire json

--- tweetstream/consumers/twitter_listener.py
+++ tweetstream/consumers/twitter_listener.py
@@ -17,7 +17,7 @@ class TwitterStreamListener(StreamListener):
         try:
             raw_data = json.loads(raw_data)
             logger.info(f"Twitter Listener sending data to sink")
-            self.sink.send(raw_data['text'].encode('utf-8'))
+            self.sink.send(json.dumps(raw_data).encode('utf-8'))
             return True
 
         except BaseException as error:
